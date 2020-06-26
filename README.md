b6467e419da28b57e35d8564dd1a03d951db1d69
Szelepcsényi Zsolt <szelepcsenyi.zsolt@e-vista.hu>
2020-06-26 10:29:45 +0200

    Init

---
+++ README.md
@@ -0,0 +1,30 @@
+# yii2-webrtc-adapter-asset
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Run
+
+```
+composer require szelepke/yii2-webrtc-adapter-asset "*"
+
+or add
+
+
+```
+"szelepke/yii2-webrtc-adapter-asset": "*"
+```
+
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+```php
+<?php
+    \szelepke\webrtcadapter\WebrtcAdapterAsset::register($this);
+?>
+```
---
+++ WebrtcAdapterAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2020
+ * @author Szelepcsényi Zsolt <szelepcsenyi.zsolt@e-vista.hu>
+ * @package yii2-webrtc-adapter-asset
+ */
+namespace szelepke\webrtcadapter;
+use yii\web\AssetBundle;
+use yii\web\View;
+/**
+ * Class VueSmoothDndAsset
+ * @package szelepke\yii2-webrtc-adapter-asset
+ */
+class WebrtcAdapterAsset extends AssetBundle
+{
+    public $sourcePath = '@npm/webrtc-adapter/dist';
+    
+    public $js = [
+        'adapter_core.js'
+    ];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
+    ];
+
+    public $depends = [
+        'szelepke\webrtcadapter\WebrtcAdapterAsset',
+    ];
+}
